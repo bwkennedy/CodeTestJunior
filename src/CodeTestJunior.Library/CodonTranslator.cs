d13b3d5de039f8bf8536e61f43a1ce876acf4967
Cade Sherwood <sherwoodcade@gmail.com>
2021-11-30 18:18:48 -0700

    Completed the CodonTranslator.cs class.

--- src/CodeTestJunior.Library/CodonTranslator.cs
+++ src/CodeTestJunior.Library/CodonTranslator.cs
@@ -1,5 +1,6 @@
 using CodonTestJunior.Builder;
 using CodonTestJunior.DataModel;
+using System.Collections.Generic;
 
 namespace CodonTestJunior.Library
 {
@@ -56,7 +57,46 @@ namespace CodonTestJunior.Library
         /// <returns>Amino acid sequence</returns>
         public string Translate(string dna)
         {
-            return "";
+            string translation = "";
+            int index;
+            List<string> codons;
+
+            if (dna.Contains("ATG"))
+            {
+                translation += "M";
+                index = dna.IndexOf("ATG") + 3;
+                codons = SplitCodons(dna.Substring(index));
+
+                foreach (string codon in codons)
+                {
+                    if (_translationMap.Stops.Contains(codon))
+                    {
+                        break;
+                    }
+
+                    translation += _translationMap.CodonMap[codon].ToString();
+                }
+            }
+
+            return translation;
+        }
+
+        /// <summary>
+        /// Split the dna substring into list of codons.
+        /// </summary>
+        /// <param name="dnaSubstring"></param>
+        /// <returns>List of codons</returns>
+        private List<string> SplitCodons(string dnaSubstring)
+        {
+            List<string> codons = new List<string>();
+            int index = 0;
+            while (index + 3 < dnaSubstring.Length)
+            {
+                codons.Add(dnaSubstring.Substring(index, 3));
+                index += 3;
+            }
+
+            return codons;
         }
     }
 }
